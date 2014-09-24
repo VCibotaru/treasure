a23c348e61bb66370f827e328e206369b2b554ae
Viktor Chibotaru <vcibotaru@gmail.com>
2014-09-24 12:41:41 +0400

    Initial Commit

---
+++ treasure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  treasure
+//
+//  Created by Viktor Chibotaru on 9/24/14.
+//  Copyright (c) 2014 Viktor Chibotaru. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
